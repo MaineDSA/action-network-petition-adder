01c065fde332dc8f9dbda0db70c6e412392c570d
Wil T <wil.t.me@pm.me>
2025-10-21 18:45:24 -0400

    add random wait and missing async

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import csv
 import logging
 from pathlib import Path
+from random import SystemRandom
 
 from patchright.sync_api import sync_playwright
 from tqdm import tqdm
@@ -8,8 +9,10 @@ from tqdm import tqdm
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+cryptogen = SystemRandom()
 
-def fill_form(page, data: dict[str, str]):
+
+async def fill_form(page, data: dict[str, str]):
     """
     Fill form fields at provided action network url with provided data.
 
@@ -20,6 +23,9 @@ def fill_form(page, data: dict[str, str]):
     # Fill in the form fields
     for key, value in data.items():
         page.fill(f"#form-{key}", value)
+
+    wait_time = cryptogen.randint(500, 3000)
+    await page.wait_for_timeout(wait_time)
 
     # Submit the form
     page.click("[type=submit]")
@@ -32,7 +38,7 @@ def fill_form(page, data: dict[str, str]):
         logger.debug("No error message appeared")
 
 
-def main():
+async def main():
     csv_path = r"CSV-PATH-HERE"
     petition_name = "PETITION-NAME-HERE"
     with sync_playwright() as p:
@@ -43,7 +49,7 @@ def main():
             csv_reader = csv.DictReader(file)
             for signer in tqdm(csv_reader):
                 page.goto(f"https://actionnetwork.org/petitions/{petition_name}?kiosk=true")
-                fill_form(page, signer)
+                await fill_form(page, signer)
 
         browser.close()
 
