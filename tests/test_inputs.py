1c78827bd112341926f7d8e6c99fcdf119e3a922
Wil T <wil.t.me@pm.me>
2025-10-30 13:09:30 -0400

    test missing source tag input

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -28,3 +28,27 @@ async def test_get_inputs() -> None:
         assert csv_path == known_csv_path
         assert action_name == known_action_name
         assert source_tag == known_source_tag
+
+
+@pytest.mark.asyncio
+async def test_get_inputs_no_source() -> None:
+    known_csv_path = Path(__file__).parent / "test_resources/example.csv"
+    known_action_name = "my-petition"
+    known_source_tag = "paper"  # noqa F841
+
+    def mock_input(prompt: str) -> str:
+        if "path to the CSV file" in prompt:
+            return str(known_csv_path)
+        if "name of the action" in prompt:
+            return known_action_name
+        if "source tag" in prompt:
+            return ""
+        msg = f"Unexpected prompt: {prompt}"
+        raise ValueError(msg)
+
+    with patch("builtins.input", side_effect=mock_input):
+        csv_path, action_name, source_tag = await get_inputs()
+
+        assert csv_path == known_csv_path
+        assert action_name == known_action_name
+        assert not source_tag
