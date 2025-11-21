8bc56452cc1457e00eee7430849280773cb10eea
Ramona T <ramona.t.me@pm.me>
2025-11-21 12:05:15 -0500

    use headless browser for testing context creation

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -2,6 +2,6 @@ from src.main import browser_context
 
 
 async def test_browser() -> None:
-    async with browser_context() as page:
+    async with browser_context(headless=True) as page:
         await page.goto("https://example.com")
         assert await page.title() == "Example Domain"
